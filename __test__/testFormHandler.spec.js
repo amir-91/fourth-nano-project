fb8ed7a91e74cea4cc8ce17ff7785813a42a58ce
amir-91 <61394176+amir-91@users.noreply.github.com>
2022-01-22 21:46:01 +0200

    edit work service code

--- __test__/testFormHandler.spec.js
+++
@@ -1,7 +0,0 @@
-import { handleSubmit } from "../src/client/js/formHandler"
- 
-describe("Testing submit functionality", () => {
-    test("Testing the handleSubmit() function", () => {
-        const url = 'www.google.com'
-        expect(url).toMatch(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g)
-})})
